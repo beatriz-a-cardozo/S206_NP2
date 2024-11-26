1dcda8d74d41e602557cc9e204f19c1a3ea156f5
Beatriz de Araújo Cardozo <102707560+beatriz-a-cardozo@users.noreply.github.com>
2024-11-26 19:07:51 -0300

    Cypress -  código com o exercício de Cypress
    Realizei 4 casos de testes (2 falhos) com o site da Wikipedia!

---
+++ CYPRESS/cypress/e2e/spec.cy.js
@@ -0,0 +1,54 @@
+describe('template spec', () => {
+
+  it('teste de pesquisas - com sucesso', () => {
+    
+    let mes = new Date().getMonth().toString()
+    let seg = new Date().getSeconds().toString()
+
+    let year = mes + seg
+
+    cy.visit('https://www.wikipedia.org')
+    cy.get('#searchInput').type(year)
+    cy.get('.pure-button').click()
+
+    cy.get('.mw-page-title-main').should('contain.text', year)
+
+  })
+
+  it('teste de troca de Idioma - com sucesso', () => {
+
+    cy.visit('https://www.wikipedia.org')
+    cy.get('#js-link-box-de').click()
+    cy.get('#Willkommen_bei_Wikipedia').should('contain.text', 'Willkommen bei  Wikipedia')
+  
+  })
+
+  it('teste de editar um arquivo - com falha', () => {
+
+    cy.visit('https://www.wikipedia.org')
+    cy.get('#searchInput').type('Percy Jackson & the Olympians')
+    cy.get('.pure-button').click()
+    cy.get('#ca-edit > a').click()
+     //cy.get('.mbox-text > span').should('have.text', 'Bem-vindo/a! Por decisão da comunidade, é necessário estar regist(r)ado/a para editar ou criar artigos na Wikipédia lusófona.')
+    login()
+    cy.get('.cdx-message__content').should('contain.text', 'Existem problemas com alguns dos dados introduzidos')
+    
+  })
+
+  it('teste de login - com falha', () => {
+
+    cy.visit('https://www.wikipedia.org')
+    cy.get('#js-link-box-pt > strong').click()
+    cy.get('#pt-login-2 > a > span').click()
+    login()
+    cy.get('.cdx-message__content').should('contain.text', 'Existem problemas com alguns dos dados introduzidos')
+
+  })
+  
+}) 
+
+function login(){
+  cy.get('#wpName1').type('Perseus Rick Jackson')
+  cy.get('#wpPassword1').type('!@#$abcd1234')
+  cy.get('#wpLoginAttempt').click()
+}
